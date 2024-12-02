4516429211da604bccacd52541a428428173fc48
onefangg <onefangg@gmail.com>
2024-12-02 21:49:23 +0800

    Add Day 3 scaffold + Remove embargo on txt file + clean day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -20,21 +20,14 @@ var remainingSafeReports = parsedInput
         var diff = report.Skip(1).Zip(shiftRight, (l, r) => r-l).ToArray();
         return !(diff.All(x => Math.Abs(x) < 4 && Math.Abs(x) > 0)
                  && (diff.All(x => x < 0) || diff.All(x => x > 0)));
-    })
-    .Where(report =>
-    {
-        var res = new List<bool> ();
-        foreach (var idx in Enumerable.Range(0, report.Length))
+    }).Count(report => Enumerable.Range(0, report.Length).Any(idx =>
         {
-            
             var excludeLevels = report.Where((l, i) => idx != i).ToArray();
             var shiftRightAgain = excludeLevels.Take(excludeLevels.Length - 1).ToArray();
             var diffAgain = excludeLevels.Skip(1).Zip(shiftRightAgain, (l, r) => r-l).ToArray();
-            res.Add(diffAgain.All(x => Math.Abs(x) < 4 && Math.Abs(x) > 0) 
-                && (diffAgain.All(x => x < 0 ) || diffAgain.All(x => x > 0 )));
+            return diffAgain.All(x => Math.Abs(x) < 4 && Math.Abs(x) > 0)
+                   && (diffAgain.All(x => x < 0) || diffAgain.All(x => x > 0));
         }
-
-        return res.Count() > 0 && res.Any(x => x == true);
-    }).Count();
+    ));
 
 Console.WriteLine($"Part 2 Input is: {remainingSafeReports + safeReports.Length}");
