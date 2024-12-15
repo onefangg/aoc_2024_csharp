a7666e68825d5bbbee6ac0ea197be34ad2dc3bf0
onefangg <onefangg@gmail.com>
2024-12-15 19:34:59 +0800

    Add Day 15 part 2 (ugly) solution

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -13,7 +13,7 @@ for (int i = 0; i < data.Length; i++)
 
 var grid = data[..splitAt].Select(x => x.ToCharArray()).ToArray();
 var directions = data[(splitAt + 1)..].SelectMany(x => x).ToArray();
-
+var wideGrid = TranslateToWideGrid(grid);
 
 var currRobotPosition = FindRobotPosition(grid);
 foreach (var dire in directions)
@@ -35,11 +35,41 @@ foreach (var dire in directions)
         var newPos = ShiftRobotPosition(grid, (1,0), currRobotPosition);
         currRobotPosition = newPos;
     }
-
 }
-VisualiseRobots(grid);
+
+// VisualiseRobots(grid);
 Console.WriteLine($"Part 1: {CalculateBoxCoordinations(grid)}");
 
+var iteration = 1;
+
+var currRobotPositionForPartTwo = FindRobotPosition(wideGrid);
+
+foreach (var dire in directions)
+{
+    (int, int) offset = (0, 0); // whack
+    if (dire == '<')
+    {
+        offset = (0,-1);
+    } else if (dire == '^')
+    {
+        offset = (-1,0);
+    } else if (dire == '>')
+    {
+        offset = (0,1);
+    } else if (dire == 'v')
+    {
+        offset = (1,0);
+    }
+    
+    var newPos = ShiftRobotPositionForPartTwo(wideGrid, offset, currRobotPositionForPartTwo);
+    currRobotPositionForPartTwo = newPos;
+    
+    // Console.WriteLine($"Iteration {iteration} and movement applied: {dire}");
+    // VisualiseRobots(wideGrid);
+    iteration++;
+}
+
+Console.WriteLine($"Part 2: {CalculateBoxCoordinations(wideGrid)}");
 void VisualiseRobots(char[][] gridData)
 {
     for (int i = 0; i < gridData.Length; i++)
@@ -58,7 +88,7 @@ long CalculateBoxCoordinations(char[][] gridData)
 
     for (int i = 1; i < gridData.Length - 1; i++)
     {
-        for (int j = 1; j < gridData[j].Length-1; j++)
+        for (int j = 1; j < gridData[i].Length-1; j++)
         {
             if (gridData[i][j] == 'O' || gridData[i][j] == '[')
             {
@@ -68,6 +98,38 @@ long CalculateBoxCoordinations(char[][] gridData)
     }
 
     return res;
+}
+
+char[][] TranslateToWideGrid(char[][] gridData)
+{
+    var matrix = new List<char[]>();
+    for (int i = 0; i < gridData.Length; i++)
+    {
+        var row = new char[gridData[i].Length * 2];
+        for (int j = 0; j < gridData[i].Length; j++)
+        {
+            if (gridData[i][j] == '#')
+            {
+                row[j*2] = '#';    
+                row[2*j+1] = '#';    
+            } else if (gridData[i][j] == '.')
+            {
+                row[j*2] = '.';    
+                row[2*j+1] = '.';
+            } else if (gridData[i][j] == 'O')
+            {
+                row[j*2] = '[';    
+                row[2*j+1] = ']';
+            }
+            else if (gridData[i][j] == '@')
+            {
+                row[j*2] = '@';    
+                row[2*j+1] = '.';
+            }
+        }
+        matrix.Add(row.ToArray());
+    }
+    return matrix.ToArray();
 }
 
 (int, int) ShiftRobotPosition(char[][] gridData, (int, int) directionOffset, (int, int) robotPosition)
@@ -112,6 +174,161 @@ long CalculateBoxCoordinations(char[][] gridData)
 
 }
 
+(int, int) ShiftRobotPositionForPartTwo(char[][] gridData, (int, int) directionOffset, (int, int) robotPosition)
+{
+    var (x, y) = robotPosition;
+    var (dx, dy) = directionOffset; 
+    var (shiftedX, shiftedY) = (x+dx, y+dy);
+    if (gridData[shiftedX][shiftedY] == '#')
+    {
+        return (x, y);
+    } else if (gridData[shiftedX][shiftedY] == '.')
+    {
+        gridData[x][y] = '.';
+        gridData[shiftedX][shiftedY] = '@';
+        return (shiftedX, shiftedY);
+    } else if (gridData[shiftedX][shiftedY] == '[' || gridData[shiftedX][shiftedY] == ']')
+    {
+        // shift left or right
+        if (dx == 0)
+        {
+            var (nextFreeX, nextFreeY) = (shiftedX + dx, shiftedY + dy);
+            while (true)
+            {
+                if (gridData[nextFreeX][nextFreeY] == '.') break;
+                else if (gridData[nextFreeX][nextFreeY] == '#')
+                {
+                    nextFreeX = shiftedX;
+                    nextFreeY = shiftedY;
+                    break;
+                }
+                nextFreeX += dx;
+                nextFreeY += dy;
+            }
+
+            if (shiftedX == nextFreeX && shiftedY == nextFreeY)
+            {
+                return (x, y);
+            }
+            
+
+            if (dy == -1)
+            {
+                // shift everything left
+                gridData[nextFreeX][nextFreeY] = '[';
+                for (int shiftLeft = nextFreeY + 1; shiftLeft < y; shiftLeft++)
+                {
+                    if (gridData[nextFreeX][shiftLeft] == ']')
+                    {
+                        gridData[nextFreeX][shiftLeft] = '[';
+                    }
+                    else
+                    {
+                        gridData[nextFreeX][shiftLeft] = ']';
+                    }
+                }
+            }
+            else
+            {
+                // shift everything right
+                gridData[nextFreeX][nextFreeY] = ']';
+                for (int shiftRight = nextFreeY - 1; shiftRight > y; shiftRight--)
+                {
+                    if (gridData[nextFreeX][shiftRight] == ']')
+                    {
+                        gridData[nextFreeX][shiftRight] = '[';
+                    }
+                    else
+                    {
+                        gridData[nextFreeX][shiftRight] = ']';
+                    }
+                }
+            }
+            gridData[x][y] = '.';
+            gridData[shiftedX][shiftedY] = '@';
+            return (shiftedX, shiftedY);
+        }
+        // up or down direction
+        else
+        {
+            var box = new Box()
+            {
+                Lhs = gridData[shiftedX][shiftedY] == '[' ? (shiftedX, shiftedY) : (shiftedX, shiftedY -1),
+                Rhs = gridData[shiftedX][shiftedY] == ']' ? (shiftedX, shiftedY) : (shiftedX, shiftedY +1),
+            };
+            var toShiftBoxes = new List<Box>() { box };
+            var (nextFreeX, nextFreeY) = (shiftedX + dx, shiftedY + dy);
+
+            while (true)
+            {
+                if (toShiftBoxes.Where(b=>b.Lhs.Item1 == nextFreeX - dx).All(b => b.DoesItClear(dx, gridData)))
+                {
+                    break;
+                }
+
+                if (toShiftBoxes.Where(b => b.Lhs.Item1 == nextFreeX - dx).Any(b => b.DidItHitObstacle(dx, gridData)))
+                {
+                    return (x, y);
+                }
+                
+                
+                toShiftBoxes.AddRange(GetConnectedBoxes(gridData, toShiftBoxes, nextFreeX, dx));
+                nextFreeX += dx;
+                
+            }
+            
+            var orderBoxes = dx == 1 ? toShiftBoxes.OrderByDescending(b => b.Lhs.Item1) : 
+                toShiftBoxes.OrderBy(b => b.Lhs.Item1);
+            
+            foreach (var singleBox in orderBoxes)
+            {
+                var (lx, ly) = singleBox.Lhs;
+                gridData[lx+dx][ly] = '[';
+                var (rx, ry) = singleBox.Rhs;
+                gridData[rx+dx][ry] = ']';
+                
+                gridData[lx][ly] = '.';
+                gridData[rx][ry] = '.';
+            }
+            
+            gridData[x][y] = '.';
+            gridData[shiftedX][shiftedY] = '@';
+            return (shiftedX, shiftedY);
+        }
+        
+    }
+    throw new Exception("I don't think this will ever happen");
+}
+
+List<Box> GetConnectedBoxes(char[][] gridData, List<Box> inputBox, int anchorX, int offsetX)
+{
+    int anchorRow = anchorX;
+    int[] yCoords = inputBox.Where(x=> (x.Lhs.Item1 == anchorX - offsetX) )
+        .Select(b => (int[])[b.Lhs.Item2, b.Rhs.Item2]).SelectMany(x=>x).ToArray();
+    var connectedBoxes = new HashSet<Box>();
+    foreach (var y in yCoords)
+    {
+        if (gridData[anchorRow][y] == '[')
+        {
+            connectedBoxes.Add(new Box()
+            {
+                Lhs = (anchorRow, y),
+                Rhs = (anchorRow, y+1),
+            });
+        }
+        else if (gridData[anchorRow][y] == ']')
+        {
+            connectedBoxes.Add(new Box()
+            {
+                Lhs = (anchorRow, y-1),
+                Rhs = (anchorRow, y),
+            });
+        }
+    }
+
+    return connectedBoxes.ToList();
+}
+
 (int, int) FindRobotPosition(char[][] gridData)
 {
     for (int i = 0; i < gridData.Length; i++)
@@ -127,10 +344,27 @@ long CalculateBoxCoordinations(char[][] gridData)
     return (-1, -1);
 }
 
+public record struct Box
+{
+    public(int, int) Lhs { get; set; }
+    public(int, int) Rhs { get; set; }
 
 
-
-
-
-
-
+    public bool DoesItClear(int offset, char[][] gridData)
+    {
+        foreach (var (boxSideX, boxSideY) in ((int, int)[])[Lhs, Rhs])
+        {
+            if (gridData[boxSideX+offset][boxSideY] != '.') return false;
+        }
+        return true;
+    }
+    
+    public bool DidItHitObstacle(int offset, char[][] gridData)
+    {
+        foreach (var (boxSideX, boxSideY) in ((int, int)[])[Lhs, Rhs])
+        {
+            if (gridData[boxSideX+offset][boxSideY] == '#') return true;
+        }
+        return false;
+    }
+}
