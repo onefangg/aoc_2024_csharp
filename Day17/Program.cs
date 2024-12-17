2998ad40218533ecda4d5ce5967c50f3c14ee5d9
onefangg <onefangg@gmail.com>
2024-12-17 20:19:20 +0800

    Add Day 17 part 1 sln

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,11 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// actual input
 var inputString = @"
-Register A: 729
+Register A: 33024962
 Register B: 0
 Register C: 0
 
-Program: 0,1,5,4,3,0";
+Program: 2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0";
+
+// var inputString = @"
+// Register A: 0
+// Register B: 2024
+// Register C: 43690
+//
+// Program: 4,0";
 
 
 var parsingInput = inputString.Split("\r\n").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
@@ -21,66 +29,73 @@ foreach (var register in parsingRegisters)
 
 var program = parsingProgram.Split(": ")[^1].Split(",").ToArray();
 
+int instructionPointer = 0;
 
-int opCode = -1;
-bool didJump = false;
-
-for (int i = 0; i < program.Length; i++)
+while (instructionPointer < program.Length)
 {
-     if (i % 2 == 0) 
-     {
-         // instruction
-         opCode = int.Parse(program[i]);
-     }
-     else
-     {
-         if (didJump)
-         {
-             didJump = false;
-             continue;
-         }
-         
-         var operand = int.Parse(program[i]);
-         if (opCode == 0)
-         {
-             var numerator = registers['A'];
-             var denominator = Math.Pow(2, operand);
-             registers['A'] = (int) Math.Truncate(numerator / denominator);
-         }
-         else if (opCode == 1)
-         {
-             registers['B'] = Convert.ToByte(registers['B']) ^ Convert.ToByte(operand);
-         }
-         else if (opCode == 2)
-         {
-             registers['B'] = operand % 8;
-         }
-         else if (opCode == 3)
-         {
-             if (registers['A'] != 0)
-             {
-                 registers['A'] = operand;
-                 didJump = true;
-             } 
-         }
-         else if (opCode == 4)
-         {
-             registers['C'] = Convert.ToByte(registers['B']) ^ Convert.ToByte(registers['C']);
-         }
-         else if (opCode == 5)
-         {
-             Console.Write($"{operand % 5},");
-         }
-         else if (opCode == 6)
-         {
-             var numerator = registers['A'];
-             var denominator = Math.Pow(2, operand);
-             registers['A'] = (int) Math.Truncate(numerator / denominator);
-         }
-         else if (opCode == 7) {}
-         
-     }
-    
+    var opCode = int.Parse(program[instructionPointer]);
+    var operand = GetValueFromOperand(char.Parse(program[instructionPointer + 1]), registers);
+    if (opCode == 0)
+    {
+        var numerator = registers['A'];
+        var denominator = Math.Pow(2, operand);
+        registers['A'] = (int)Math.Truncate(numerator / denominator);
+    }
+    else if (opCode == 1)
+    {
+        registers['B'] ^= int.Parse(program[instructionPointer + 1]);
+    }
+    else if (opCode == 2)
+    {
+        registers['B'] = operand % 8;
+    }
+    else if (opCode == 3)
+    {
+        if (registers['A'] != 0)
+        {
+            instructionPointer = int.Parse(program[instructionPointer + 1]);
+            continue;
+        }
+    }
+    else if (opCode == 4)
+    {
+        registers['B'] ^= registers['C'];
+    }
+    else if (opCode == 5)
+    {
+        Console.Write($"{operand % 8},");
+    }
+    else if (opCode == 6)
+    {
+        var numerator = registers['A'];
+        var denominator = Math.Pow(2, operand);
+        registers['B'] = (int)Math.Truncate(numerator / denominator);
+    }
+    else if (opCode == 7)
+    {
+        var numerator = registers['A'];
+        var denominator = Math.Pow(2, operand);
+        registers['C'] = (int)Math.Truncate(numerator / denominator);
+    }
+
+    instructionPointer += 2;
 }
 
-Console.WriteLine(program[0]);
+
+int GetValueFromOperand(char operand, Dictionary<char, int> registers)
+{
+    switch (operand)
+    {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+            return Int32.Parse(operand.ToString());
+        case '4': return registers['A'];
+        case '5': return registers['B'];
+        case '6': return registers['C'];
+    }
+
+    // shoulnd't happen
+    return -1;
+}
