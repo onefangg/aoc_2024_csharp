6e7867bbb881afd70ab5b3541e6a69acf115622e
onefangg <onefangg@gmail.com>
2024-12-19 22:34:32 +0800

    Add Day 19 pt 1

---
+++ Day19/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var data = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day19.txt")).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+var availTowels = data[0].Split(", ").ToArray();
+var desiredPatterns = data[1..];
+
+var cnt = 0;
+foreach (var design in desiredPatterns)
+{
+    var iterateThrough = new SortedSet<string>(Comparer<string>.Create((a,b)=>a.Length-b.Length));
+    iterateThrough.Add(design);
+    
+    while (iterateThrough.Count > 0)
+    {
+        var matchAgainst = iterateThrough.Min()!;
+        iterateThrough.Remove(matchAgainst);
+        
+        if (matchAgainst == "")
+        {
+            cnt++;
+            break;
+        }
+        foreach (var towel in availTowels)
+        {
+            if (matchAgainst.StartsWith(towel))
+            {
+                iterateThrough.Add(matchAgainst[towel.Length..]);
+            }
+        }
+
+    }
+}
+Console.WriteLine(cnt);
