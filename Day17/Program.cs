5c00d4f3638c18370702d4efe27fb81d1e3fb621
onefangg <onefangg@gmail.com>
2024-12-22 19:41:17 +0800

    Mild rewrite of Day 17

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -17,18 +17,17 @@ Program: 0,3,5,4,3,0";
 
 var parsingInput = inputString.Split("\r\n").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
 var (parsingRegisters, parsingProgram) = (parsingInput[..3], parsingInput[3]);
-var inputRegisters = new Dictionary<char, long>();
-foreach (var register in parsingRegisters)
+var registers = parsingRegisters.Select(x =>
 {
-    var splitRegister = register.Split(": ");
-    var registerName = splitRegister[0];
-    inputRegisters[registerName[^1]] = Int32.Parse(splitRegister[1]);
-}
+    var splitRegister = x.Split(": ");
+    return long.Parse(splitRegister[1]);
+}).ToArray();
+
 var inputProgramString = parsingProgram.Split(": ")[^1];
-var outputProgramString = RunProgram(inputRegisters, inputProgramString);
+var outputProgramString = RunProgram(registers[0],registers[1], registers[2], inputProgramString);
 Console.WriteLine($"{outputProgramString}");
 
-string RunProgram(Dictionary<char, long> register, string programStr)
+string RunProgram(long a, long b, long c, string programStr)
 {
     var output = new List<char>();
     var program = programStr.Split(",").ToArray();
@@ -37,55 +36,25 @@ string RunProgram(Dictionary<char, long> register, string programStr)
     while (instructionPointer < program.Length)
     {
         var opCode = int.Parse(program[instructionPointer]);
-        var operand = GetValueFromOperand(char.Parse(program[instructionPointer + 1]), register);
-        if (opCode == 0)
+        var operand = GetValueFromOperand(char.Parse(program[instructionPointer + 1]), a, b,c);
+        if (opCode == 0) a = (long)Math.Truncate(a / Math.Pow(2, operand));
+        else if (opCode == 1) b^= long.Parse(program[instructionPointer + 1]);
+        else if (opCode == 2) b = operand % 8;
+        else if (opCode == 3 && a != 0)
         {
-            var numerator = register['A'];
-            var denominator = Math.Pow(2, operand);
-            register['A'] = (long)Math.Truncate(numerator / denominator);
+            instructionPointer = int.Parse(program[instructionPointer + 1]);
+            continue;
         }
-        else if (opCode == 1)
-        {
-            register['B'] ^= long.Parse(program[instructionPointer + 1]);
-        }
-        else if (opCode == 2)
-        {
-            register['B'] = operand % 8;
-        }
-        else if (opCode == 3)
-        {
-            if (register['A'] != 0)
-            {
-                instructionPointer = int.Parse(program[instructionPointer + 1]);
-                continue;
-            }
-        }
-        else if (opCode == 4)
-        {
-            register['B'] ^= register['C'];
-        }
-        else if (opCode == 5)
-        {
-            output.Add(char.Parse((operand % 8).ToString()));
-        }
-        else if (opCode == 6)
-        {
-            var numerator = register['A'];
-            var denominator = Math.Pow(2, operand);
-            register['B'] = (long)Math.Truncate(numerator / denominator);
-        }
-        else if (opCode == 7)
-        {
-            var numerator = register['A'];
-            var denominator = Math.Pow(2, operand);
-            register['C'] = (long)Math.Truncate(numerator / denominator);
-        }
+        else if (opCode == 4) b ^= c;
+        else if (opCode == 5) output.Add(char.Parse((operand % 8).ToString()));
+        else if (opCode == 6) b = (long)Math.Truncate(a / Math.Pow(2, operand));
+        else if (opCode == 7) c = (long)Math.Truncate(a / Math.Pow(2, operand));
         instructionPointer += 2;
     }
     return String.Join(",", output);
 }
 
-long GetValueFromOperand(char operand, Dictionary<char, long> registers)
+long GetValueFromOperand(char operand, long a, long b, long c)
 {
     switch (operand)
     {
@@ -94,10 +63,9 @@ long GetValueFromOperand(char operand, Dictionary<char, long> registers)
         case '2':
         case '3':
             return long.Parse(operand.ToString());
-        case '4': return registers['A'];
-        case '5': return registers['B'];
-        case '6': return registers['C'];
+        case '4': return a;
+        case '5': return b;
+        case '6': return c;
     }
-    // shoulnd't happen
     return -1;
 }
