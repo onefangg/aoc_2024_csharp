8b7d7165675b3fdaaa8f3db6ce8e38742e94eb36
onefangg <onefangg@gmail.com>
2024-12-25 22:30:57 +0800

    Add Day 23 Part 2 solution

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-var data = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day23.txt"));
+﻿var data = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day23.txt"));
 var network = data.Select(x=> x.Split('-')).ToList();
 
 var networkMap = new Dictionary<string, HashSet<string>>();
@@ -19,44 +16,72 @@ foreach (var node in network)
     }
 }
 
-
-var connectedToThree = new List<string[]>();
+var lookup = new Dictionary<string, int[]>();
 foreach (var (node, neighbours) in networkMap)
 {
-    foreach (var n in neighbours)
+    // if co - de and co - ka and de -ka
+    // from the perspective of co, de and ka connecting to each counts them as 2\
+    var arr = neighbours.ToArray();
+    lookup[node] = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
     {
-        if (!networkMap.ContainsKey(n))
-        {
-            continue;
-        }
-        networkMap.TryGetValue(n, out HashSet<string> indirectNeighbours);
-        var sameNeighbours = indirectNeighbours.Intersect(neighbours).ToArray();
-        if (sameNeighbours.Count() >= 1)
-        {
-
-            foreach (var n2 in sameNeighbours)
-            {
-                string[] constructMap = [node, n, n2];
-                constructMap = constructMap.OrderBy(x => x).ToArray();
-                if (!connectedToThree.Any(x => x.SequenceEqual(constructMap)))
-                {
-                    connectedToThree.Add(constructMap);    
-                }    
-            }
-            
-            
-        }
+        var anchor = arr[i];
+        var otherNeighbours = arr.Where(x => x != anchor).ToArray();
+        var anchorNeighbours = networkMap[anchor].Where(x=>x != node).ToArray();
+        
+        lookup[node][i] = anchorNeighbours.Intersect(otherNeighbours).Count();    
     }
 }
 
-// var distinctThrees = connectedToThree
-//     .DistinctBy(x => new HashSet<(string,string,string)>([(x.Item1, x.Item2, x.Item3)])).ToArray();
-//
-//
-// var anyStartsWithT = 
-//     distinctThrees.Where(x => new List<string>([x.Item1, x.Item2, x.Item3])
-//         .Any(x => x.StartsWith("t"))).ToArray();
+var g =  lookup
+    .MaxBy(x => {
+        var maxV = x.Value.Max();
+        return x.Value.Count(y=>y == maxV);    
+    }
+    ).Key;
+var num = lookup[g];
+var maxrelation = num.Max();
 
-Console.WriteLine($"{connectedToThree.Count(x => x.Any(y => y.StartsWith("t")))}");
+var relevant = new List<string>([g]);
+for (int i = 0; i < networkMap[g].Count; i++)
+{
+    if (num[i] == maxrelation)
+    {
+        relevant.Add(networkMap[g].ToArray()[i]);
+    }
+}
 
+GetPart1Solution(networkMap);
+Console.WriteLine($"Part 2: {string.Join(",", relevant.OrderBy(x=>x))}");
+
+void GetPart1Solution(Dictionary<string, HashSet<string>> inputNetworkMap)
+{
+    var connectedToThree = new List<string[]>();
+    foreach (var (node, neighbours) in inputNetworkMap)
+    {
+        foreach (var n in neighbours)
+        {
+            if (!inputNetworkMap.ContainsKey(n))
+            {
+                continue;
+            }
+            inputNetworkMap.TryGetValue(n, out HashSet<string> indirectNeighbours);
+            var sameNeighbours = indirectNeighbours.Intersect(neighbours).ToArray();
+            if (sameNeighbours.Any())
+            {
+                foreach (var n2 in sameNeighbours)
+                {
+                    string[] constructMap = [node, n, n2];
+                    constructMap = constructMap.OrderBy(x => x).ToArray();
+                    if (!connectedToThree.Any(x => x.SequenceEqual(constructMap)))
+                    {
+                        connectedToThree.Add(constructMap);    
+                    }    
+                }
+            }
+        }
+    }
+
+    Console.WriteLine($"{connectedToThree.Count(x => x.Any(y => y.StartsWith("t")))}");
+}
 
