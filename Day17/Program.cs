398b2d13912ce1a8fe36f38fad40d630ef814a45
onefangg <onefangg@gmail.com>
2024-12-28 21:06:03 +0800

    Add Day17 part 2 solution and Day 21 fix project

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,19 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // actual input
-// var inputString = @"
-// Register A: 33024962
-// Register B: 0
-// Register C: 0
-//
-// Program: 2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0";
-
 var inputString = @"
-Register A: 117440
+Register A: 216584205979245
 Register B: 0
 Register C: 0
 
-Program: 0,3,5,4,3,0";
+Program: 2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0";
+
+// var inputString = @"
+// Register A: 117440
+// Register B: 0
+// Register C: 0
+//
+// Program: 0,3,5,4,3,0";
 
 var parsingInput = inputString.Split("\r\n").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
 var (parsingRegisters, parsingProgram) = (parsingInput[..3], parsingInput[3]);
@@ -23,26 +23,52 @@ var registers = parsingRegisters.Select(x =>
     return long.Parse(splitRegister[1]);
 }).ToArray();
 
-var inputProgramString = parsingProgram.Split(": ")[^1];
-var outputProgramString = RunProgram(registers[0],registers[1], registers[2], inputProgramString);
-Console.WriteLine($"{outputProgramString}");
+var inputProgram = parsingProgram.Split(": ")[^1].Split(",").Select(long.Parse).ToArray();
+var outputProgramString = RunProgram(registers[0],registers[1], registers[2], inputProgram);
+Console.WriteLine($"Part 1: {outputProgramString}");
 
-string RunProgram(long a, long b, long c, string programStr)
+ReverseEngineerProgram(0, 0, inputProgram);
+
+// neurons connected thanks to this: https://publish.reddit.com/embed?url=https://www.reddit.com/r/adventofcode/comments/1hhtc6g/comment/m2u1fjh/
+void ReverseEngineerProgram(long a, long iter, long[] outputProgram)
+{
+    if (RunProgram(a, 0, 0, outputProgram) == string.Join(",", outputProgram))
+    {
+        Console.WriteLine($"Part 2: a is {a}");
+        return;
+    } 
+    var xIterOutput = string.Join(",",outputProgram[Convert.ToInt32(outputProgram.Length -  iter)..]);
+    var progOutput = RunProgram(a, 0, 0, outputProgram);
+    
+    if (progOutput == xIterOutput || iter == 0)
+    {
+        for (int i =0;i < 8; i++)
+        {
+            ReverseEngineerProgram(a * 8 + i, iter+1, outputProgram);
+        }
+    } 
+}
+
+string RunProgram(long a, long b, long c, long[] program)
 {
     var output = new List<char>();
-    var program = programStr.Split(",").ToArray();
-    
-    int instructionPointer = 0;
+    long instructionPointer = 0;
     while (instructionPointer < program.Length)
     {
-        var opCode = int.Parse(program[instructionPointer]);
-        var operand = GetValueFromOperand(char.Parse(program[instructionPointer + 1]), a, b,c);
+        var opCode = program[instructionPointer];
+        var operand = GetValueFromOperand(program[instructionPointer + 1], a, b,c);
+        
+        // Console.WriteLine($"value of opcode: {opCode}");
+        // Console.WriteLine($"value of a: {a}");
+        // Console.WriteLine($"value of a in binary: {Convert.ToString(a, 2)}");
+        // Console.WriteLine($"value of b: {b}");
+        // Console.WriteLine($"value of c: {c}");
         if (opCode == 0) a = (long)Math.Truncate(a / Math.Pow(2, operand));
-        else if (opCode == 1) b^= long.Parse(program[instructionPointer + 1]);
+        else if (opCode == 1) b^= program[instructionPointer + 1];
         else if (opCode == 2) b = operand % 8;
         else if (opCode == 3 && a != 0)
         {
-            instructionPointer = int.Parse(program[instructionPointer + 1]);
+            instructionPointer = program[instructionPointer + 1];
             continue;
         }
         else if (opCode == 4) b ^= c;
@@ -54,18 +80,18 @@ string RunProgram(long a, long b, long c, string programStr)
     return String.Join(",", output);
 }
 
-long GetValueFromOperand(char operand, long a, long b, long c)
+long GetValueFromOperand(long operand, long a, long b, long c)
 {
     switch (operand)
     {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
+        case 0:
+        case 1:
+        case 2:
+        case 3:
             return long.Parse(operand.ToString());
-        case '4': return a;
-        case '5': return b;
-        case '6': return c;
+        case 4: return a;
+        case 5: return b;
+        case 6: return c;
     }
     return -1;
 }
