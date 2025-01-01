0cea3a5bed2610d75b95f15933883c4b183356ee
onefangg <onefangg@gmail.com>
2025-01-01 20:52:40 +0800

    Rough attempt to fix Day 9

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,83 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
+
 var data = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "day9.txt"))[0].ToCharArray().Select(x =>x.ToString()).ToArray();
 var diskMap = new List<string>();
-var currRunningNumber = 0;
-var shouldFlipToFreeMemory = false;
-
 for (int i = 0; i < data.Length; i++)
 {
-    var repeatN = Int32.Parse(data[i]);
-    var addMemory = currRunningNumber.ToString();
-    for (int n = 0; n < repeatN; n++)
+    if (i % 2 == 0)
     {
-        if (shouldFlipToFreeMemory)
-        {
-            diskMap.Add(".");
-        }
-        else
-        {
-            diskMap.Add(addMemory);
-        }
-
-        
-    }
-
-    if (!shouldFlipToFreeMemory) currRunningNumber++;
-    shouldFlipToFreeMemory = !shouldFlipToFreeMemory;
-}
-
-
-
-int FindNonFreeMemoryIndexFromTheBack(List<string> inputDiskMap, int startFromindex)
-{
-    for (int i = startFromindex; i > 0; i--)
-    {
-        if (inputDiskMap[i] != ".") return i;
-    }
-    //DUD
-    return -1;
-}
-
-var updateDiskMap = new List<string>();
-var end = FindNonFreeMemoryIndexFromTheBack(diskMap, diskMap.Count()-1);
-var shiftedIndices = new List<int>();
-
-for (int idx = 0; idx < diskMap.Count; idx++)
-{
-    if (shiftedIndices.Contains(idx) || idx > end)
-    {
-        updateDiskMap.Add(".");
-        continue;
-    }
-
-    if (diskMap[idx] != ".")
-    {
-        updateDiskMap.Add(diskMap[idx]);
+        diskMap.AddRange(Enumerable.Repeat(Math.Ceiling((double)i/2).ToString(CultureInfo.InvariantCulture), int.Parse(data[i])));
     }
     else
     {
-        updateDiskMap.Add(diskMap[end]);
-        shiftedIndices.Add(end);
-        end  = FindNonFreeMemoryIndexFromTheBack(diskMap, end - 1);
+        diskMap.AddRange(Enumerable.Repeat(".", int.Parse(data[i])));
     }
 }
 
-var res = (double) 0;
-var diskMapJoin = string.Join("", updateDiskMap);
 
-for (int i = 0; i < updateDiskMap.Count(); i++)
+var diskMapArray = diskMap.ToArray();
+
+for (int i = diskMapArray.Length - 1; i >= 0; i--)
 {
-    if (updateDiskMap[i] == ".")
+    if (diskMapArray[i] != ".")
     {
-        break;
-        
+        for (int j = 0; j < i; j++)
+        {
+            if (diskMapArray[j] == ".")
+            {
+                diskMapArray[j] = diskMapArray[i];
+                diskMapArray[i] = ".";
+                break;
+            }
+        }
     }
-    res += (double)(i * Int64.Parse(updateDiskMap[i].ToString()));
 }
 
+Console.WriteLine($"Part 1: {GetChecksum(diskMapArray)}");
+int GetChecksum(string[] disk)
+{
+    var checksum = 0;
+    for (int i = 0; i < disk.Length; i++)
+    {
+        if (disk[i] != ".")
+        {
+            checksum += i * int.Parse(disk[i]);
+        }
+    }
 
-
-// Console.WriteLine($"{string.Join("", updateDiskMap)}");
-Console.WriteLine($"{res.ToString("F0")}");
-
+    return checksum;
+}
